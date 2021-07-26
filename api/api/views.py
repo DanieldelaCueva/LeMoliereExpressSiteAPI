0b22279267ab9c14e00112342104f4b23f980b0f
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2021-07-26 18:23:51 +0200

    Update views.py

--- api/api/views.py
+++ api/api/views.py
@@ -4,22 +4,22 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
-        'Articles': {
-            'Create': '/articles/article-create/',
+        '/articles': {
+            'Create': '/articles/article-create/ [AUTHENTICATION REQUIRED]',
             'List': '/articles/article-list/',
-            'Detail': '/articles/article-detail/<int:pk>',
-            'Update': '/articles/article-update/<int:pk>',
-            'Delete': '/articles/article-delete/<int:pk>',
-            'Validate': '/articles/article-validate/<int:pk>',
-            'Invalidate': '/articles/article-invalidate/<int:pk>'
+            'Detail': '/articles/article-detail/<int:pk>/',
+            'Update': '/articles/article-update/<int:pk>/ [AUTHENTICATION REQUIRED]',
+            'Delete': '/articles/article-delete/<int:pk>/ [AUTHENTICATION REQUIRED]',
+            'Validate': '/articles/article-validate/<int:pk>/ [AUTHENTICATION REQUIRED]',
+            'Invalidate': '/articles/article-invalidate/<int:pk>/ [AUTHENTICATION REQUIRED]'
         },
 
-        'Authorization': {
+        '/authorization': {
             'Login': '/authorization/login/',
-            'Logout': '/authorization/logout/',
-            'User List': '/authorization/user-list',
-            'User Detail' : '/authorization/user-detail/<str:username>',
-            'Change Password': '/authorization/change-password/<str:username>'
+            'Logout': '/authorization/logout/ [AUTHENTICATION REQUIRED]',
+            'User List': '/authorization/user-list/',
+            'User Detail' : '/authorization/user-detail/<str:username>/',
+            'Change Password': '/authorization/change-password/<str:username>/ [AUTHENTICATION REQUIRED]'
         }
     }
 
