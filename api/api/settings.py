04a39c997fe9567e94ea86ddd91fd2b40662a75f
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2021-08-21 20:56:01 +0200

    cors

--- api/api/settings.py
+++ api/api/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'articles',
     'authorization',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +141,4 @@ REST_FRAMEWORK = {
     # ]
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
